06a640cb7d7053c7216ffcc81035323df2ea0b5e
Edwin Agbenyega <eddy.devv@gmail.com>
2019-03-17 00:26:21 -0600

    Add twilio for phone number verification in front end only to realize it works mostly for backend servers. Need to find a substitute for now

--- src/components/Landing/LandingContainer.js
+++ src/components/Landing/LandingContainer.js
@@ -14,16 +14,36 @@ class LandingContainer extends Component {
         },
         {
           id: "qwdqwd1",
-          phoneNumber: "50523234123",
+          phoneNumber: "5052323412",
           nickname: "aycarl"
         }
       ],
       x: 0,
       y: 0,
-      customStyle: {}
+      twilio_client: {},
+      current_number: ''
     }
 
     this.handleMouseOver = this.handleMouseOver.bind(this)
+  }
+
+  componentDidMount = () => {
+    const accountSid = process.env.REACT_APP_TWILIO_ACCOUNT_SID
+    const authToken = process.env.REACT_APP_TWILIO_AUTH_TOKEN
+    // const client = require('twilio')(accountSid, authToken)
+    // this.setState({
+    //   twilio_client: client
+    // })
+    console.log(authToken)
+  }
+
+  lookupNumber = phone => {
+    const client = this.state.twilio_client
+    let number = ''
+    client.lookups.phoneNumbers(phone).fetch({countryCode: 'US'}).then(phone_number => { number = phone_number.nationalFormat })
+    this.setState({
+      current_number: number
+    })
   }
 
   handleMouseEnter = e => {
