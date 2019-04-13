971ac8113c3fdc492b3d5469da39a040ace50e73
Edwin Agbenyega <eddy.devv@gmail.com>
2019-04-13 00:06:00 -0600

    Add github pages

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
 import LandingContainer from '../components/Landing/LandingContainer'
 import WelcomeContainer from '../components/Welcome/WelcomeContainer'
-import { Route, Switch } from 'react-router-dom'
+import { HashRouter as Router, Route, Switch } from 'react-router-dom'
 import '../stylesheets/App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Switch>
-          <Route exact path="/" component={LandingContainer} />
-          <Route exact path="/welcome" component={WelcomeContainer} />
-        </Switch>
-        
-      </div>
+      <Router>
+        <div className="App">
+          <Switch>
+            <Route exact path="/" component={LandingContainer} />
+            <Route exact path="/welcome" component={WelcomeContainer} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
