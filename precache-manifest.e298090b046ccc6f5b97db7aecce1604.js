62b12d9eeb4430f5f205c8ef99c408a02080ebe6
Edwin Agbenyega <eddybanks03@gmail.com>
2020-05-02 20:28:22 -0600

    Updates

---
+++ precache-manifest.e298090b046ccc6f5b97db7aecce1604.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64f1545e56559e2a3d9108fd03499384",
+    "url": "/hangout229/index.html"
+  },
+  {
+    "revision": "0c274d6e5a52aae0f65c",
+    "url": "/hangout229/static/css/2.52bb6724.chunk.css"
+  },
+  {
+    "revision": "a940ea8aa49208b488a3",
+    "url": "/hangout229/static/css/main.33c00b7a.chunk.css"
+  },
+  {
+    "revision": "0c274d6e5a52aae0f65c",
+    "url": "/hangout229/static/js/2.4ccd18ee.chunk.js"
+  },
+  {
+    "revision": "a940ea8aa49208b488a3",
+    "url": "/hangout229/static/js/main.6f9e0ce2.chunk.js"
+  },
+  {
+    "revision": "781c0d9376990aa0724f",
+    "url": "/hangout229/static/js/runtime-main.6f060d82.js"
+  },
+  {
+    "revision": "e6e10cdfdc537b2f65efcdf9d6aac988",
+    "url": "/hangout229/static/media/logoPurple.e6e10cdf.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/hangout229/precache-manifest.8a6c4620e34f6c199b753e3aa90f8b87.js"
+  "/hangout229/precache-manifest.e298090b046ccc6f5b97db7aecce1604.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/hangout229/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hangout229/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
